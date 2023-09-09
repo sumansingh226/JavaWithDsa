5b48b38f8d67cab9fb9b235f0dbc660e25facc5f
Suman Singh <99945059+sumansingh6degreeit@users.noreply.github.com>
2023-09-09 20:09:49 +0530

    getters and setter for accessing private values

---
+++ src/OOPS/Books.java
@@ -0,0 +1,18 @@
+package OOPS;
+
+public class Books {
+    String bookName ;
+    double prize;
+    private  String author ;
+
+    public  String getAuthor() // getter method for access author name outside of class
+    {
+        return  author;
+    }
+
+    public  void  setAuthor(String name)
+    {
+        author = name;
+        return;
+    }
+}
---
+++ src/OOPS/GettersAndSettersInJava.java
@@ -0,0 +1,16 @@
+package OOPS;
+
+public class GettersAndSettersInJava {
+
+
+    public static void main(String[] args) {
+
+        Books b1 = new Books();
+
+//        for  set author name
+    b1.setAuthor("Suman singh");
+    //  for geta author name
+       String authorName = b1.getAuthor();
+        System.out.println(authorName);
+    }
+}
