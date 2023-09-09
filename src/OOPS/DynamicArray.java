8966b75cc359af3bef032c74bd2b657c959dc8be
Suman Singh <99945059+sumansingh6degreeit@users.noreply.github.com>
2023-09-10 00:57:17 +0530

    Dynamic arrays  / make own ArrayList

---
+++ src/OOPS/DynamicArray.java
@@ -0,0 +1,31 @@
+package OOPS;
+import java.util.Arrays;
+
+public class DynamicArray {
+
+    public static class ArrayList {
+        int[] arr = new int[2];
+        int idx =0;
+        int size =0;
+        public void add(int element)
+        {
+            if(size == arr.length)
+            {
+                int[] newArray = Arrays.copyOf(arr, arr.length*2);
+                arr =newArray;
+            }
+            arr[idx] = element;
+            idx++;
+            size++;
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+         ArrayList arr =  new ArrayList();
+         arr.add(5);
+         arr.add(6);
+         arr.add(2);
+  }
+}
