4d1c4582afc455b37a202f123ca4f13741f0e6fa
SUMAN_SINGH-SDE-1 <suman.singh@softsages.com>
2023-09-11 20:05:57 +0530

    remove extra space

--- src/OOPS/Constructor.java
+++ src/OOPS/Constructor.java
@@ -9,12 +9,10 @@ public class Constructor {
           double percentage;
         static int numberOfStudents;
           final String  schoolName = "GICG"; // for making anything constant
-
             public  Student()  //default constructor
             {
-
             }
-          public   Student(String name, int age, double percentage) {
+          public Student(String name, int age, double percentage) {
                this.age = age ;
                this.name = name;
                this.percentage = percentage;
@@ -35,6 +33,7 @@ public class Constructor {
         System.out.println(sc.age + " "+ sc.name + " "+ sc.percentage);
         Student sc1 = new Student(18, "suman", 70.6);
         Student sc6 = new Student(18,"suman",  70.6);
+        Student sc7 = new Student(18,"suman",  70.6);
         System.out.println(sc.schoolName);
         System.out.println(sc6.numberOfStudents);
     }
