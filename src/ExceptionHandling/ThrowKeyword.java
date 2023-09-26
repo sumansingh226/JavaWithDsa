e1a215ca44e9950fc0eeec8b8356eef2159080a2
SUMAN_SINGH-SDE-1 <suman.singh@softsages.com>
2023-09-26 20:54:28 +0530

    throw keyword in  exception  handling

---
+++ src/ExceptionHandling/ThrowKeyword.java
@@ -0,0 +1,35 @@
+package ExceptionHandling;
+
+import java.util.Scanner;
+
+public class ThrowKeyword {
+
+
+     public static  void deviceByZero()
+     {
+
+         try{
+             Scanner sc  = new Scanner((System.in));
+             System.out.println( "Enter Age : ");
+             int age  =sc.nextInt() ;
+             if(age<18)
+             {
+                 throw  new Exception("you are not eligible for voting");
+
+               }
+             else
+             {
+                 System.out.println("You are eligible for voting");
+             }
+
+         }
+         catch (Exception ex)
+         {
+               ex.printStackTrace();
+         }
+     }
+    public static void main(String[] args) {
+        ThrowKeyword.deviceByZero();
+        System.out.println("Function terminated by JVM");
+    }
+}
