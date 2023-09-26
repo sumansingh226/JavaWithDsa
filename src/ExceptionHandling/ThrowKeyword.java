18d0b1a1ce12918e0622ee9604cce8f1a328a98c
SUMAN_SINGH-SDE-1 <suman.singh@softsages.com>
2023-09-26 21:00:06 +0530

    throw keyword in  exception  handling

--- src/ExceptionHandling/ThrowKeyword.java
+++ src/ExceptionHandling/ThrowKeyword.java
@@ -1,34 +1,32 @@
 package ExceptionHandling;
-
 import java.util.Scanner;
-
 public class ThrowKeyword {
 
+    static class YoungerAgeException extends RuntimeException {
+        YoungerAgeException(String msg) {
+            super(msg);
+        }
+    }
 
-     public static  void deviceByZero()
-     {
-
-         try{
-             Scanner sc  = new Scanner((System.in));
-             System.out.println( "Enter Age : ");
-             int age  =sc.nextInt() ;
-             if(age<18)
-             {
-                 throw  new Exception("you are not eligible for voting");
-
-               }
-             else
-             {
-                 System.out.println("You are eligible for voting");
-             }
-
-         }
-         catch (Exception ex)
-         {
-               ex.printStackTrace();
-         }
-     }
     public static void main(String[] args) {
-        ThrowKeyword.deviceByZero();
+        ThrowKeyword throwKeyword = new ThrowKeyword();
+        throwKeyword.divideByZero();
+    }
+
+    public void divideByZero() {
+        try {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter Age : ");
+            int age = sc.nextInt();
+            if (age < 18) {
+                throw new YoungerAgeException("You are not eligible for voting");
+            } else {
+                System.out.println("You are eligible for voting");
+            }
+        } catch (YoungerAgeException ex) {
+            System.out.println(ex.getMessage());
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 }
