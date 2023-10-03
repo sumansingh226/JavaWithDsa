a8fde0f97e58dedcb47d36d1415be02f320afd2b
SUMAN_SINGH-SDE-1 <suman.singh@softsages.com>
2023-10-03 20:20:06 +0530

    Collection framework

--- src/CollectionFrameWork/Collection/CollectionDemo.java
+++ src/CollectionFrameWork/Collection/CollectionDemo.java
@@ -7,11 +7,11 @@ public class CollectionDemo {
 
         ArrayList arrayList =  new ArrayList();
         arrayList.add("1");
-        arrayList.add("23");
-        arrayList.addAll(arrayList);
-        arrayList.remove("23");
+        arrayList.add("23"); //add one value to array
+        arrayList.addAll(arrayList); // add one object to other object or add all the values of one object to other object
+        arrayList.remove("23"); // remove one value from arrayList
         System.out.println(arrayList);
-        arrayList.removeAll(arrayList);
+        arrayList.removeAll(arrayList); // remove one object to other object or add all the values of one object to other object
         System.out.println(arrayList);
     }
 }
