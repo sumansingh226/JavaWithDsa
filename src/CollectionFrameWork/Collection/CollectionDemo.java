15deef45014702fe9fe7816b63960fd089016c4c
SUMAN_SINGH-SDE-1 <suman.singh@softsages.com>
2023-10-03 20:27:36 +0530

    Collection framework

--- src/CollectionFrameWork/Collection/CollectionDemo.java
+++ src/CollectionFrameWork/Collection/CollectionDemo.java
@@ -10,10 +10,11 @@ public class CollectionDemo {
         arrayList.add("23"); //add one value to array
         arrayList.addAll(arrayList); // add one object to other object or add all the values of one object to other object
         arrayList.remove("23"); // remove one value from arrayList
-
+        System.out.println(arrayList.isEmpty()); // is collection object empty
         System.out.println(arrayList.contains("23")); // is it contain a value
         arrayList.removeAll(arrayList); // remove one object to other object or add all the values of one object to other object
         System.out.println(arrayList);
         System.out.println(arrayList.contains("23"));
+        System.out.println(arrayList.isEmpty()); // is collection object empty
     }
 }
