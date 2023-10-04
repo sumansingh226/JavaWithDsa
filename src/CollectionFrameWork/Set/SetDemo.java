fdf7f5a684b400559c52e39e59793f38afe67d75
SUMAN_SINGH-SDE-1 <suman.singh@softsages.com>
2023-10-04 21:05:12 +0530

    set implementation

---
+++ src/CollectionFrameWork/Set/SetDemo.java
@@ -0,0 +1,17 @@
+package CollectionFrameWork.Set;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetDemo {
+
+
+    public static void main(String[] args) {
+        Set s = new HashSet();
+        s.add("0123");
+        s.add("suman01233");
+        System.out.println("IsEmpty "  +    s.isEmpty());
+        System.out.println("Set is : " + s);
+
+    }
+}
