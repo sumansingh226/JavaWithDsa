72069249dfcc9e0dd1b526ead00df464bcf804c0
SUMAN_SINGH-SDE-1 <suman.singh@softsages.com>
2023-10-04 20:56:40 +0530

    List demo

---
+++ src/CollectionFrameWork/List/ListDemo.java
@@ -0,0 +1,14 @@
+package CollectionFrameWork.List;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListDemo {
+
+
+    public static void main(String[] args) {
+
+        List al = new ArrayList<Integer>(); // create a reference of List from using object ArrayList
+        al.add("1");
+        System.out.println(al);
+    }
+}
