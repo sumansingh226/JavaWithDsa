9787f126517c841131920155030e4f311c4ee5cf
SUMAN_SINGH-SDE-1 <suman.singh@softsages.com>
2023-10-09 19:57:13 +0530

    Enumeration in Java Collection

---
+++ src/CollectionFrameWork/Enumerations/EnumerationDemo.java
@@ -0,0 +1,15 @@
+package CollectionFrameWork.Enumerations;
+
+import java.util.Vector;
+
+public class EnumerationDemo {
+
+
+    public static void main(String[] args) {
+
+        Vector v  = new Vector();
+        v.add(1);
+        v.add(2);
+        System.out.println(v);
+    }
+}
