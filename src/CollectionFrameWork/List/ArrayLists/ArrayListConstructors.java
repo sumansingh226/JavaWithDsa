fadf7a854683228092be4eb7a8916052fb6c00ef
SUMAN_SINGH-SDE-1 <suman.singh@softsages.com>
2023-10-11 20:26:49 +0530

    Enumeration in Java Collection

--- src/CollectionFrameWork/List/ArrayLists/ArrayListConstructors.java
+++ src/CollectionFrameWork/List/ArrayLists/ArrayListConstructors.java
@@ -1,6 +1,7 @@
 package CollectionFrameWork.List.ArrayLists;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class ArrayListConstructors {
 
@@ -10,9 +11,16 @@ public class ArrayListConstructors {
         al.add(12);
         al.add(123);
         al.add(1234);
-        System.out.println("al + " +  al);
+        System.out.println("al : " +  al);
         ArrayList al2 = new ArrayList(al);
-        System.out.println("al2 + " +  al2);
+        System.out.println("al2 : " +  al2);
         System.out.println(al2.get(2));
+        System.out.println(  al.indexOf(123));
+//       al.removeAll(al);
+        Iterator i = al.iterator();
+        while (i.hasNext())
+        {
+            System.out.println("i"  + i.next());
+        }
     }
 }
