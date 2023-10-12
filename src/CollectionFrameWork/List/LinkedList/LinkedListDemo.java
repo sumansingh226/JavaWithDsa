b48418f6fa48f7d9df955e3216c5c18b962167d7
SUMAN_SINGH-SDE-1 <suman.singh@softsages.com>
2023-10-12 18:52:07 +0530

    LinkedList in Java Collection

--- src/CollectionFrameWork/List/LinkedList/LinkedListDemo.java
+++ src/CollectionFrameWork/List/LinkedList/LinkedListDemo.java
@@ -1,12 +1,28 @@
 package CollectionFrameWork.List.LinkedList;
+import java.util.Iterator;
 import java.util.LinkedList;
-import java.util.List;
+import java.util.ListIterator;
 
 public class LinkedListDemo {
 
 
     public static void main(String[] args) {
-        List li = new LinkedList();
-        System.out.println(li);
+        LinkedList ll =  new LinkedList();
+        ll.add("first-Node");  // add an element to linked-list
+        ll.add("second-Node");  // add an element to linked-list
+        ll.add(3);  // add an element to linked-list
+        ll.add('S');  // add an element to linked-list
+        ll.add(3);  // add an element to linked-list   (it can store duplicate values)
+        ll.add(null);  // add a null element to linked-list
+        ll.add(null);  // add a duplicate null element to linked-list
+        System.out.println(ll);
+
+        // follow the iteration order (it means in which order you oder you inserted element it will iterate in same way)
+        Iterator itr = ll.iterator();
+
+        while (itr.hasNext())
+        {
+            System.out.println("itr " + itr.next());
+        }
     }
 }
