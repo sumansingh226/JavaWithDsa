4aa441d4a87555de064c07fd8b4a54eab1c51d65
SUMAN_SINGH-SDE-1 <suman.singh@softsages.com>
2023-10-12 19:28:06 +0530

    LinkedList in Java Collection

--- src/CollectionFrameWork/List/LinkedList/LinkedListDemo.java
+++ src/CollectionFrameWork/List/LinkedList/LinkedListDemo.java
@@ -15,7 +15,7 @@ public class LinkedListDemo {
         ll.add(3);  // add an element to linked-list   (it can store duplicate values)
         ll.add(null);  // add a null element to linked-list
         ll.add(null);  // add a duplicate null element to linked-list
-        System.out.println(ll);
+        System.out.println("linked-list-1 " +ll);
 
         // follow the iteration order (it means in which order you oder you inserted element it will iterate in same way)
         Iterator itr = ll.iterator();
@@ -24,5 +24,36 @@ public class LinkedListDemo {
         {
             System.out.println("itr " + itr.next());
         }
+
+        // add all the elements of ll  to ll2 using addAll() method
+        LinkedList ll2 =  new LinkedList();
+        ll2.addAll(ll);
+
+        System.out.println("linked-list-2 " +ll2);
+
+        // remove an element from list
+          ll.remove(0);
+        System.out.println("list after remove from index 0 " + ll);
+
+        // check size of linked list object
+        System.out.println("size : "  + ll.size());
+
+         // contains method to check  if object contain a particular element or not
+        System.out.println("contains " + ll.contains(3));
+
+        // contain all method to check all elements contain or not
+        System.out.println( "containsAll " +ll2.containsAll(ll));
+
+        // remove all elements from collection object or  list
+        ll.removeAll(ll);
+        System.out.println("remove all elements from list " + ll);
+
+
+         // clear method to remove all elements from linked list
+        ll2.clear();
+        System.out.println("clear , "+ ll2);
+
+        // check if a list  object is empty or not
+        System.out.println( "isEmpty : " + ll.isEmpty());
     }
 }
