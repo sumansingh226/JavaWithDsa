7c4422c89c511631f587a30cb3c93b5539bfa021
SUMAN_SINGH-SDE-1 <suman.singh@softsages.com>
2023-10-12 18:33:19 +0530

    Enumeration in Java Collection

---
+++ src/CollectionFrameWork/List/LinkedList/LinkedList.java
@@ -0,0 +1,12 @@
+package CollectionFrameWork.List.LinkedList;
+
+import java.util.List;
+
+public class LinkedList {
+
+
+    public static void main(String[] args) {
+        List li = new java.util.LinkedList();
+        System.out.println(li);
+    }
+}
