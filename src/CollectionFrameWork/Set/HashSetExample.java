92e2edbeda715e39f9c4fa544b14ce997f292223
SUMAN_SINGH-SDE-1 <suman.singh@softsages.com>
2023-10-17 16:10:57 +0530

    HashSet Java Collection

---
+++ src/CollectionFrameWork/Set/HashSetExample.java
@@ -0,0 +1,15 @@
+package CollectionFrameWork.Set;
+
+import java.util.HashSet;
+
+public class HashSetExample {
+
+
+    public static void main(String[] args) {
+        HashSet hs =  new HashSet();
+         hs.add("suman");
+         hs.add("chauhan");
+
+        System.out.println(hs);
+    }
+}
