cb067034f7482c7fff51646399d082bc3d2b7290
SUMAN_SINGH-SDE-1 <suman.singh@softsages.com>
2023-10-18 19:04:39 +0530

    HashMap in  Java Collection

---
+++ src/CollectionFrameWork/Map/HashMapExample.java
@@ -0,0 +1,19 @@
+package CollectionFrameWork.Map;
+
+import java.util.HashMap;
+
+public class HashMapExample {
+
+
+    public static void main(String[] args) {
+        HashMap hm = new HashMap();
+        hm.put(0 ,"10");
+        hm.put(1,"suman singh");
+        hm.put(2,2);
+        hm.put(3,2.2);
+        hm.put(4,false);
+        hm.put(5,null);
+        System.out.println("HashMap = " + hm);
+
+    }
+}
