7fc219b88e3fb639103ea2ad519a338b207c4902
SUMAN_SINGH-SDE-1 <suman.singh@softsages.com>
2023-10-18 19:10:07 +0530

    HashMap in  Java Collection

--- src/CollectionFrameWork/Map/HashMapExample.java
+++ src/CollectionFrameWork/Map/HashMapExample.java
@@ -1,6 +1,9 @@
 package CollectionFrameWork.Map;
 
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
 public class HashMapExample {
 
@@ -14,6 +17,18 @@ public class HashMapExample {
         hm.put(4,false);
         hm.put(5,null);
         System.out.println("HashMap = " + hm);
+
+        Set set = hm.entrySet();
+        System.out.println("set = " + set);
+        Iterator itr = set.iterator();
+        while (itr.hasNext())
+        {
+
+            Map.Entry entry = (Map.Entry) itr.next();
+            System.out.println("entry keys " + entry.getKey());
+            System.out.println("entry values " + entry.getValue());
+        }
+
 
     }
 }
