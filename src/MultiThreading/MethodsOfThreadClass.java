dac0c7513004bc78357b8552820a8b502c2d95c8
SUMAN_SINGH-SDE-1 <suman.singh@softsages.com>
2023-10-25 20:20:14 +0530

    Methods of Thread class

---
+++ src/MultiThreading/MethodsOfThreadClass.java
@@ -0,0 +1,13 @@
+package MultiThreading;
+
+public class MethodsOfThreadClass extends Thread {
+
+
+
+
+
+    public static void main(String[] args) {
+        System.out.println("CurrentThreadName " + Thread.currentThread() );
+
+    }
+}
