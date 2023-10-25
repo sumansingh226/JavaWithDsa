acf39173b3fcb79c2bf75c067639dde9bc5dbc5d
SUMAN_SINGH-SDE-1 <suman.singh@softsages.com>
2023-10-25 20:36:11 +0530

    Methods of Thread class

--- src/MultiThreading/MethodsOfThreadClass.java
+++ src/MultiThreading/MethodsOfThreadClass.java
@@ -2,16 +2,22 @@ package MultiThreading;
 
 public class MethodsOfThreadClass extends Thread {
 
-
-
+    public void run() {
+        System.out.println("instance Thread running");
+    }
 
 
     public static void main(String[] args) {
 
-        System.out.println("CurrentThread " + Thread.currentThread() ); // to get currentThread
-        System.out.println("CurrentThreadName " + Thread.currentThread().getName() ); // to get currentThreadName
-         Thread.currentThread().setName("mainThread1") ; // set current Thread name
-        System.out.println("CurrentThreadName " + Thread.currentThread().getName() ); // to get currentThreadName
+        System.out.println("CurrentThread " + Thread.currentThread()); // to get currentThread
+        System.out.println("CurrentThreadName " + Thread.currentThread().getName()); // to get currentThreadName
+        Thread.currentThread().setName("suman singh's Thread"); // set current Thread name
+        System.out.println("CurrentThreadName " + Thread.currentThread().getName()); // to get currentThreadName
+
+        MethodsOfThreadClass mte = new MethodsOfThreadClass();
+        mte.setName("InstanceThread");
+        System.out.println("mte  " + mte.getName());
+        mte.start();
 
     }
 }
