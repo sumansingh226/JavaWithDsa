98cc7a3eafdab651e94c7a394a417bf6d2f40234
SUMAN_SINGH-SDE-1 <suman.singh@softsages.com>
2023-10-26 20:06:05 +0530

    Daemon Thread in Java

--- src/MultiThreading/DaemonThreadExample.java
+++ src/MultiThreading/DaemonThreadExample.java
@@ -1,27 +1,13 @@
 package MultiThreading;
 
 public class DaemonThreadExample {
+
+
     public static void main(String[] args) {
         Thread daemonThread = new Thread(() -> {
-            while (true) {
-                System.out.println("Daemon thread is running");
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
+            System.out.println("daemon Thread");
         });
-
-        daemonThread.setDaemon(true);
-        daemonThread.start();
-
-        try {
-            Thread.sleep(3000); // Sleep for 3 seconds
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        System.out.println("Main thread is exiting");
+        daemonThread.setDaemon(true);  // set a normal Thread as daemon Thread
+        System.out.println("is Thread daemon Thread : " + daemonThread.isDaemon());
     }
 }
