dec3c7f2dfcecaa080b9317c7e382462e65f25fa
SUMAN_SINGH-SDE-1 <suman.singh@softsages.com>
2023-11-01 20:03:45 +0530

    thread interrupt in java multithreading

--- src/MultiThreading/InterruptionExample.java
+++ src/MultiThreading/InterruptionExample.java
@@ -4,10 +4,13 @@ public class InterruptionExample extends Thread {
     public void run() {
         try {
             for (int i = 0; i < 5; i++) {
+                System.out.println(Thread.currentThread().getName() + " ie.isInterrupted() : " + Thread.currentThread().isInterrupted());
                 System.out.println("i is : " + i);
                 if (i == 3) {
                     Thread.sleep(1000);
+
                 }
+
             }
         } catch (Exception ex) {
             System.out.println(ex);
@@ -17,7 +20,9 @@ public class InterruptionExample extends Thread {
     public static void main(String[] args) {
         InterruptionExample ie = new InterruptionExample();
         ie.start();
+        System.out.println(Thread.currentThread().getName() + " is  : " + ie.isInterrupted());
         ie.interrupt();
+        System.out.println(Thread.currentThread().getName() + " is  : " + ie.isInterrupted());
 
     }
 }
