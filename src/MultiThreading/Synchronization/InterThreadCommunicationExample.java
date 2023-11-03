4cf8d0eeb74a074469f7b37d265468c535bfda1a
SUMAN_SINGH-SDE-1 <suman.singh@softsages.com>
2023-11-03 20:49:25 +0530

    Static Synchronization in Java Multithreading

--- src/MultiThreading/Synchronization/InterThreadCommunicationExample.java
+++ src/MultiThreading/Synchronization/InterThreadCommunicationExample.java
@@ -1,18 +1,16 @@
 package MultiThreading.Synchronization;
 
-class  TotalEarnings extends  Thread
-{
-    int total =0;
-    public void run()
-    {
-        for (int i =0; i<=10;i++)
-        {
-            total = total+10;
+class TotalEarnings extends Thread {
+    int total = 0;
+
+    public void run() {
+        for (int i = 0; i <= 10; i++) {
+            total = total + 10;
         }
     }
 }
 
-public  class InterThreadCommunicationExample  {
+public class InterThreadCommunicationExample {
 
     public static void main(String[] args) {
         TotalEarnings te = new TotalEarnings();
